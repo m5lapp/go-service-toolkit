39de3546a2c9e8e0a7075e1d77271c804736e417
m5lapp <git@m5l.app>
2023-06-06 12:59:14 +0100

    Testing returning a configuration struct from the Flags function.

--- config/config.go
+++ config/config.go
@@ -14,11 +14,11 @@ type Cors struct {
 func (c *Cors) Flags() {
 	flag.Func(
 		"cors-trusted-origins",
-	    "Trusted CORS origins (space seperated)",
+		"Trusted CORS origins (space seperated)",
 		func(val string) error {
 			c.trustedOrigins = strings.Fields(val)
 			return nil
-	    },
+		},
 	)
 }
 
@@ -32,7 +32,7 @@ type Limiter struct {
 // Flags parses the flags configured for a rate limiter. The parameters it
 // takes are the default values to use for the rps, burst and active flags
 // respectively.
-func (l* Limiter) Flags(rps float64, burst int, active bool) {
+func (l *Limiter) Flags(rps float64, burst int, active bool) {
 	flag.Float64Var(&l.rps, "limiter-rps", rps, "Rate limiter max requests per second")
 	flag.IntVar(&l.burst, "limiter-burst", burst, "Rate limiter max burst per second")
 	flag.BoolVar(&l.active, "limiter-active", active, "Activate rate limiter")
@@ -65,6 +65,24 @@ func (s *Server) Flags(addr string) {
 	flag.StringVar(&s.env, "env", "development", "Environment (development|staging|production)")
 }
 
+// Smtp stores the configuration for an SMTP server connection.
+type Smtp struct {
+	host     string
+	port     int
+	username string
+	password string
+	sender   string
+}
+
+// Flags parses the flags for an SMTP server connection.
+func (s *Smtp) Flags(host, sender string) {
+	flag.StringVar(&s.host, "smtp-host", host, "SMTP host")
+	flag.IntVar(&s.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&s.username, "smtp-username", "", "SMTP username")
+	flag.StringVar(&s.password, "smtp-password", "", "SMTP password")
+	flag.StringVar(&s.sender, "smtp-sender", sender, "SMTP sender, format: Name <email@address.com>")
+}
+
 // SqlDB stores the configuration for a SQL database.
 type SqlDB struct {
 	dsn          string
@@ -81,4 +99,16 @@ func (s *SqlDB) Flags(open, idle int, idleTime string) {
 	flag.IntVar(&s.maxOpenConns, "db-max-open-conns", open, "Database max open connections")
 	flag.IntVar(&s.maxIdleConns, "db-max-idle-conns", idle, "Database max idle connections")
 	flag.StringVar(&s.maxIdleTime, "db-max-idle-time", idleTime, "Database max connection idle time (time.Duration)")
+}
+
+// Flags parses the flags for a SQL database. The parameters it takes are
+// for the default max open connections, max idle connections and max idle
+// connection times respectively.
+func SqlDbFlags(open, idle int, idleTime string) SqlDB {
+	var s SqlDB
+	flag.StringVar(&s.dsn, "db-dsn", "", "Database DSN (Data Source Name)")
+	flag.IntVar(&s.maxOpenConns, "db-max-open-conns", open, "Database max open connections")
+	flag.IntVar(&s.maxIdleConns, "db-max-idle-conns", idle, "Database max idle connections")
+	flag.StringVar(&s.maxIdleTime, "db-max-idle-time", idleTime, "Database max connection idle time (time.Duration)")
+	return s
 }
